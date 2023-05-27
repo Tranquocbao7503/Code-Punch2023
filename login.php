2470989013c4e4fbc4cc8a82125907bbffbc6684
N1ghtf4ll <tri345345@gmail.com>
2023-05-27 22:57:42 +0700

    login and wellcome

--- login.php
+++ login.php
@@ -24,9 +24,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Đăng nhập thất bại
         echo "Tên đăng nhập hoặc mật khẩu không đúng.";
     }
-
+    header("location: wellcome.php");
     // Đóng prepared statement và kết nối
     $stmt->close();
     $conn->close();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+    <h2>Đăng nhập</h2>
+    <form method="post" action="login.php">
+        <label>Tên đăng nhập:</label>
+        <input type="text" name="username" required><br><br>
+        <label>Mật khẩu:</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" value="Đăng nhập">
+    </form>
+</body>
+</html>
