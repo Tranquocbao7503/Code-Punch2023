d4d87699f796d477ab6e71843eae88d9528cd8ee
N1ghtf4ll <tri345345@gmail.com>
2023-05-27 22:49:24 +0700

    fix sql injection

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+// Kết nối tới cơ sở dữ liệu
+$servername = "localhost";
+$username_db = "root";
+$password_db = "";
+$dbname = "codeandpunch";
+$conn = new mysqli($servername, $username_db, $password_db, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối tới cơ sở dữ liệu thất bại: " . $conn->connect_error);
+}
+?>
--- login.php
+++ login.php
@@ -6,22 +6,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = $_POST['password'];
 
     // Kết nối tới cơ sở dữ liệu
-    $servername = "localhost";
-    $username_db = "root";
-    $password_db = "";
-    $dbname = "codeandpunch";
-    $conn = new mysqli($servername, $username_db, $password_db, $dbname);
+    require 'connect.php';
 
-    // Kiểm tra kết nối
-    if ($conn->connect_error) {
-        die("Kết nối tới cơ sở dữ liệu thất bại: " . $conn->connect_error);
-    }
-
-    // Chuẩn bị truy vấn SQL
-    $query = "SELECT * FROM users WHERE username = '$username' AND password = '$password'";
-
-    // Thực thi truy vấn
-    $result = $conn->query($query);
+    // Chuẩn bị truy vấn SQL với prepared statement
+    $query = "SELECT * FROM users WHERE username = ? AND password = ?";
+    $stmt = $conn->prepare($query);
+    $stmt->bind_param('ss', $username, $password);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
     // Kiểm tra kết quả truy vấn
     if ($result->num_rows == 1) {
@@ -33,27 +25,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         echo "Tên đăng nhập hoặc mật khẩu không đúng.";
     }
 
-    // Đóng kết nối
+    // Đóng prepared statement và kết nối
+    $stmt->close();
     $conn->close();
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-</head>
-<body>
-    <h2>Đăng nhập</h2>
-    <form method="post" action="login.php">
-        <label>Tên đăng nhập:</label>
-        <input type="text" name="username" required><br><br>
-        <label>Mật khẩu:</label>
-        <input type="password" name="password" required><br><br>
-        <input type="submit" value="Đăng nhập">
-    </form>
-</body>
-</html>
